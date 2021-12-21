957c8d7f4bb00327a172a240856f8b5d3d8ba32c
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-12-21 10:35:12 -0300

    retirando a tela de configurações da lista de teste e substituindo a tela atual

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -7,6 +8,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Configurações'),
       ),
+      drawer: MainDrawer(),
       body: const Center(
         child: Text('Configurações!'),
       ),
